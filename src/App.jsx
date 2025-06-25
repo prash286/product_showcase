30302c63308f3baa6a93c2a74978f09bceafb3b7
prash286 <pr.yesankar@gmail.com>
2025-06-25 12:03:09 +0530

    Project setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomePage } from "./pages/HomePage";
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <HomePage />,
+    },
+  ]);
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
